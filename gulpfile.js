d6618cf2fe4516b7998b8043399512930a8c591d
Anton Chuvirau <antonchuvirov@icloud.com>
2020-05-20 12:00:37 +0300

    Git init

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const combine = require('stream-combiner2').obj;
+
+sass.compiler = require('node-sass');
+
+gulp.task('styles', function(){
+    return gulp.src('app/sass/*.scss')
+    .pipe(combine(sass(), concat('styles.css')))
+    .pipe(clean())
+    .pipe(gulp.dest('public/css/'))
+});
+
+gulp.task('scripts', function(){
+    return gulp.src(['app/js/plugins.js', 'app/js/main.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js/'))
+});
+
+gulp.task('clean', function(){
+    return del('public');
+});
+
+gulp.task('assets', function(){
+    return gulp.src('app/assets/**', {since: gulp.lastRun('assets')})
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts', 'assets')));
+
+gulp.task('watch', function(){
+    gulp.watch('app/sass/**/*.*', gulp.series('styles'));
+    gulp.watch('app/js/**/*.*', gulp.series('scripts'));
+    gulp.watch('app/assets/**/*.*', gulp.series('assets'));
+});
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: 'public'
+    });
+    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
