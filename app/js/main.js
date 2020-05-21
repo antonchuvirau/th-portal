6c0d0d2039a153a0f41e641f524843e41448b6f7
Anton Chuvirau <antonchuvirov@icloud.com>
2020-05-21 16:46:52 +0300

    Added privacy checkbox script

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+//Removing disabled attribute from button
+document.body.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target.tagName.toLowerCase() === 'input' && target.classList.contains('privacy-checkbox__input')) {
+        if (target.checked) {
+            target.parentElement.nextElementSibling.removeAttribute('disabled');
+        }
+        else {
+            target.parentElement.nextElementSibling.setAttribute('disabled', 'disabled');
+        }
+    }
+});
